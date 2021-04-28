f281f5acdcd8a4a267be038c3b3ca76f619d41a3
frankie <frankie@cyc.sg>
2021-04-28 19:13:15 +0800

    first GET route

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [{name: 'Jack'}, {name: 'Jill'}];
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'Successfully ggot users. Nice!',
+        users: mockUserData
+    });
+});
+
 app.listen(8000, function() {
     console.log("Server is running");
 });
