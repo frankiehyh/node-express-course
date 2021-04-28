c3faede1ab783b49a5ec7629df75b17cb1977c2b
Frankie <frankie@cyc.sg>
2021-04-28 23:42:32 +0800

    Fixed error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const mockUserData = [{name: 'Mark'}, {name: Jill}];
+const mockUserData = [{name: 'Mark'}, {name: 'Jill'}];
 
 app.get('/Users', function(req, res) {
     res.json({
