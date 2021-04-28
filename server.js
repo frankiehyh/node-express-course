501371245528c1e6fe6870769311fe06f8b3ca65
Frankie <frankie@cyc.sg>
2021-04-28 23:41:53 +0800

    added first GET

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [{name: 'Mark'}, {name: Jill}];
+
+app.get('/Users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        user: mockUserData
+    });
+});
+
 app.listen(8000, function(req, res) {
     console.log("Server is running");
 });
